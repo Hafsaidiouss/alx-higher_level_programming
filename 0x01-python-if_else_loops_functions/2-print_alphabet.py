798c9f273dd74d16dfc9674ce025a4c300f2b11d
hafsaidiousse <hafsaidiousse@gmail.com>
2023-06-06 15:16:37 -0700

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for x in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
-    print(x,end='')
-
+for x in range(97, 123):
+    print(chr(x), end='')
