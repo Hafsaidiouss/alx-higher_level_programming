b7d882eff1428c3563bf2084881e3439c80b1148
hafsaidiousse <hafsaidiousse@gmail.com>
2023-06-08 13:51:18 -0700

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    ls = dir(hidden_4)
+    ls.sort
+    n = len(ls)
+    for i in range(n):
+        if ls[i][0:2] != "__":
+            print("{}".format(ls[i]))
+    
