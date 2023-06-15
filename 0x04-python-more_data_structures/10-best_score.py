1f3b0ad8ce7561594fd758bd5fdb6b14826410cc
hafsaidiousse <hafsaidiousse@gmail.com>
2023-06-15 03:20:31 -0700

    commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    h_key = None
+    m = 0
+    for key in a_dictionary.keys():
+        if a_dictionary[key] >= m:
+            m = a_dictionary[key]
+            h_key = key
+    return h_key
